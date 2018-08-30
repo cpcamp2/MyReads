32b56d98e742ad60fb197a21a3afc5ea9ed1f77d
Chris Camp <cpcamp2@gmail.com>
2018-08-30 12:25:33 -0400

    create basic SearchPage component w/o any functionality

---
+++ src/components/SearchPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SearchPage extends Component {
+  render() {
+    return (
+      <div>This is my Search Page</div>
+    );
+  }
+}
+
+export default SearchPage;
