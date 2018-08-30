5cf3befc244bd0de72d2ad5bfe5f7a19401daca5
Chris Camp <cpcamp2@gmail.com>
2018-08-30 16:06:27 -0400

    Create and pass handleShelfChange function down to child components

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Book from './Book';
 
-const BookShelf = ({title, books}) => (
+const BookShelf = ({title, books, handleShelfChange}) => (
   <div className="bookshelf">
     <h2 className="bookshelf-title">{title}</h2>
     <div className="bookshelf-books">
       <ol className="books-grid">
-        {books.map(book => <Book key={book.id} book={book}/>)}
+        {books.map(book => <Book key={book.id} book={book} handleShelfChange={handleShelfChange}/>)}
       </ol>
     </div>
   </div>
