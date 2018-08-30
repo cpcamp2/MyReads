a7ad96e4f878aed40eea0a531909449374b46747
Chris Camp <cpcamp2@gmail.com>
2018-08-30 13:12:12 -0400

    create book component and import component to bookshelf

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Book from './Book';
 
 const BookShelf = ({title}) => (
   <div className="bookshelf">
     <h2 className="bookshelf-title">{title}</h2>
     <div className="bookshelf-books">
       <ol className="books-grid">
+        <Book />
       </ol>
     </div>
   </div>
